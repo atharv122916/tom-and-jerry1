80c1aa6d1e5c62d2321b7d09b3692ea041c75169
atharv122916 <83113926+atharv122916@users.noreply.github.com>
2021-06-26 18:16:44 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,49 @@
 
+var bg;
+var cat,catImg1,catImg2,catImg3;
+var mouse,mouseImg1,mouseImg2,mouseImg3;
+
 function preload() {
     //load the images here
+    bg = loadImage("images/garden.png");
+
+    catImg1 = loadAnimation("images/cat1.png");
+    catImg2 = loadAnimation("images/cat2.png","images/cat3.png");
+    catImg3 = loadAnimation("images/cat4.png");
+
+    mouseImg1 = loadAnimation("images/mouse1.png");
+    mouseImg2 = loadAnimation("images/mouse2.png","images/mouse3.png");
+    mouseImg3 = loadAnimation("images/mouse4.png");
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
-
+    cat = createSprite(870,600);
+    cat.addAnimation("catSitting",catImg1);
+    cat.scale = 0.2;
+    
+    mouse = createSprite(200,600);
+    mouse.addAnimation("mouseStanding",mouseImg1);
+    mouse.scale  = 0.15;
 }
 
 function draw() {
 
-    background(255);
+    background(bg);
     //Write condition here to evalute if tom and jerry collide
+    if(cat.x - mouse.x < (cat.width - mouse.width)/2)
+    { 
+        cat.velocityX=0;
+        cat.addAnimation("catLastImage", catImg3);
+        cat.x =300;
+        cat.changeAnimation("catLastImage");
 
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.changeAnimation("mouseLastImage");
+
+
+ }
     drawSprites();
 }
 
@@ -21,6 +51,18 @@ function draw() {
 function keyPressed(){
 
   //For moving and changing animation write code here
+  if(keyCode === LEFT_ARROW){
+    mouse.addAnimation("mouseLastImage", mouseImg2);
+    mouse.changeAnimation("mouseLastImage");
+    mouse.frameDelay = 25;
+
+    cat.addAnimation("catLastImage", catImg2);
+    cat.changeAnimation("catLastImage");
+    cat.velocityX = -5; 
 
 
 }
+}
+
+
+
